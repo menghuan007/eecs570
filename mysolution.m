806fed9008395079a050529413a6e2b8b0dee4e6
Lifan Gong <golifan@umich.edu>
2016-03-16 23:07:17 -0400

    modified:   mysolution.m

--- mysolution.m
+++ mysolution.m
@@ -25,20 +25,26 @@ type
 
   VCType: VC0..NumVCs-1;
 
-  MessageType: enum {  ReadReq,					-- request for data / exclusivity
-                       ReadAck,					-- read ack (w/ data)
+  MessageType: enum {  GetS,
+                       GetM,
                                              
-				       WBReq,					-- writeback request (w/ data)
-				       WBAck,					-- writeback ack 
-                           
-                       RecallReq 				-- Request & invalidate a valid copy
+				       PutS,
+				       PutM,
+				       Put_Ack,
+                       
+                       Inv,
+                       Inv_Ack,
+                       
+                       Data
                     };
 
   Message:
     Record
       mtype: MessageType;
       src: Node;
-      -- do not need a destination for verification; the destination is indicated by which array entry in the Net the message is placed
+      -- do not need a destination for verification;
+      -- the destination is indicated by which array 
+      -- entry in the Net the message is placed
       vc: VCType;
       val: Value;
     End;
@@ -46,7 +52,7 @@ type
   HomeState:
     Record
       state: enum { H_I, H_S, H_M, 					--stable states
-      				H_Pending }; 					--transient states during recall
+      				H_S_D }; 						--transient states
       owner: Node;	
       sharers: multiset [ProcCount] of Node;		--No need for sharers in this protocol, but this is a good way to represent them
       val: Value; 
@@ -66,10 +72,13 @@ type
 var
   HomeNode:  HomeState;
   Procs: array [Proc] of ProcState;
-  Net:   array [Node] of multiset [NetMax] of Message;  -- One multiset for each destination - messages are arbitrarily reordered by the multiset
-  InBox: array [Node] of array [VCType] of Message;		-- If a message is not processed, it is placed in InBox, blocking that virtual channel
+  Net:   array [Node] of multiset [NetMax] of Message;
+  -- One multiset for each destination - messages are arbitrarily reordered by the multiset
+  InBox: array [Node] of array [VCType] of Message;
+  -- If a message is not processed, it is placed in InBox, blocking that virtual channel
   msg_processed: boolean;
-  LastWrite: Value; -- Used to confirm that writes are not lost; this variable would not exist in real hardware
+  LastWrite: Value; 
+  -- Used to confirm that writes are not lost; this variable would not exist in real hardware
 
 ----------------------------------------------------------------------
 -- Procedures
@@ -128,7 +137,8 @@ Begin
     then
       if n != rqst
       then 
-        -- Send invalidation message here 
+        -- Send invalidation message here
+        Send(Inv, HomeNode.sharers[i], rqst, VC0, rqst.val);
       endif;
     endif;
   endfor;
@@ -152,53 +162,88 @@ Begin
   msg_processed := true;
 
   switch HomeNode.state
-  case H_Invalid:
+  case H_I:
     switch msg.mtype
-
-    case ReadReq:
-      HomeNode.state := H_Valid;
+	
+	case GetS:
+      HomeNode.state := H_S;
       HomeNode.owner := msg.src;
-      Send(ReadAck, msg.src, HomeType, VC1, HomeNode.val);
-
+      Send(Data, msg.src, HomeType, VC1, HomeNode.val);
+    case GetM:
+      HomeNode.state := H_M;
+      HomeNode.owner := msg.src;
+      Send(Data, msg.src, HomeType, VC1, HomeNode.val);
+    case PutS:
+      HomeNode.owner := msg.src;
+      Send(Put_Ack, msg.src, HomeType, VC1, HomeNode.val);
+	case PutM:
+	
     else
       ErrorUnhandledMsg(msg, HomeType);
 
     endswitch;
 
-  case H_Valid:
+  case H_S:
     Assert (IsUndefined(HomeNode.owner) = false) 
        "HomeNode has no owner, but line is Valid";
 
     switch msg.mtype
-    case ReadReq:
-      HomeNode.state := HT_Pending;     
+    case GetS:
       Send(RecallReq, HomeNode.owner, HomeType, VC0, UNDEFINED);
       HomeNode.owner := msg.src; --remember who the new owner will be
             
-    case WBReq:
+    case GetM:
     	assert (msg.src = HomeNode.owner) "Writeback from non-owner";
       HomeNode.state := H_Invalid;
       HomeNode.val := msg.val;
       Send(WBAck, msg.src, HomeType, VC1, UNDEFINED);
       undefine HomeNode.owner
 
+	case PutS:
+      Send(RecallReq, HomeNode.owner, HomeType, VC0, UNDEFINED);
+      HomeNode.owner := msg.src; --remember who the new owner will be
+    case PutM:
+      Send(RecallReq, HomeNode.owner, HomeType, VC0, UNDEFINED);
+      HomeNode.owner := msg.src; --remember who the new owner will be
     else
       ErrorUnhandledMsg(msg, HomeType);
 
     endswitch;
 
-  case HT_Pending:
+  case H_M:
     switch msg.mtype
    
-    case WBReq:
+    case GetS:
       Assert (!IsUnDefined(HomeNode.owner)) "owner undefined";
       HomeNode.state := H_Valid;
       HomeNode.val := msg.val;
       Send(ReadAck, HomeNode.owner, HomeType, VC1, HomeNode.val);
 
-    case ReadReq:
+    case GetM:
+    	msg_processed := false; -- stall message in InBox
+    case PutS:
+    
+    case PutM:
+
+    else
+      ErrorUnhandledMsg(msg, HomeType);
+
+  case H_S_D:
+    switch msg.mtype
+   
+    case GetS:
+      Assert (!IsUnDefined(HomeNode.owner)) "owner undefined";
+      HomeNode.state := H_Valid;
+      HomeNode.val := msg.val;
+      Send(ReadAck, HomeNode.owner, HomeType, VC1, HomeNode.val);
+
+    case GetM:
     	msg_processed := false; -- stall message in InBox
 
+	case PutS:
+	case PutM:
+	case Data:
+	
     else
       ErrorUnhandledMsg(msg, HomeType);
 
@@ -219,7 +264,7 @@ Begin
   alias pv:Procs[p].val do
 
   switch ps
-  case P_Valid:
+  case P_I:
 
     switch msg.mtype
     case RecallReq:
@@ -243,7 +288,7 @@ Begin
     endswitch;
 
 
-  case PT_WritebackPending:    
+  case P_IS_D:
 
     switch msg.mtype
     case WBAck:
@@ -255,6 +300,73 @@ Begin
     else
       ErrorUnhandledMsg(msg, p);
 		endswitch;
+  
+  case P_IM_AD:
+  
+  	switch msg.mtype
+  	case GetS:
+  	
+  	case GetM:
+  	
+  	case Data:
+  	
+  	case Inv_Ack:
+  
+  case P_IM_A:
+  	switch msg.mtype
+  	case GetS:
+  	
+  	case GetM:
+  	
+  	case Inv_Ack:
+  
+  case P_S:
+  	switch msg.mtype
+  	case Inv:
+  
+  case P_SM_AD:
+  	switch msg.mtype
+  	case GetS:
+  	
+  	case GetM:
+  	
+  	case Inv:
+  	
+  	case Data:
+  	
+  	case Inv_Ack:
+  
+  case P_SM_A:
+  	switch msg.mtype
+  	case GetS:
+  	
+  	case GetM:
+  	
+  	case Inv_Ack:
+  
+  case P_M:
+  	switch msg.mtype
+  	case GetS:
+  	
+  	case GetM:
+  
+  case P_MI_A:
+  	switch msg.mtype
+  	case GetS:
+  	
+  	case GetM:
+  	
+  	case Put_Ack:
+  
+  case P_SI_A:
+  	switch msg.mtype
+  	case Inv:
+  	
+  	case Put_Ack:
+  
+  case P_II_A:
+  	switch msg.mtype
+  	case Put_Ack:
 
   ----------------------------
   -- Error catch
